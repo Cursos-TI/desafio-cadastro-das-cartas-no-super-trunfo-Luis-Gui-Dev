c93ec70aa7c727b6731c5c38202476ab2c0c4d4a
Luis Guilherme Pinheiro <luisguipb07@gmail.com>
2025-01-19 01:51:56 +0000

    add: criar variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,8 +5,6 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
-
-int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
@@ -18,5 +16,22 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+int main() {
+    /* Irei fazer de só um dos estados, quando eu tiver a aula de laços de repeticão, Arrey e Structs, o código ficará melhor.
+    Eu tenho a base da programação mas ainda não sei sobre C, por enquanto vou abrir variavel pra cada um.*/ 
+
+    // Nomes do País e dos Estados
+    char nomePais[50], nomeEstadoA[50]; 
+    // Nomes das Cidades (divididas por numeração)
+    char nomeCidadeA01[50], nomeCidadeA02[50], nomeCidadeA03[50], nomeCidadeA04[50];
+    // População das Cidades
+    float popuA01, popuA02, popuA03, popuA04;
+    // PIB
+    float pibA01, pibA02, pibA03, pibA04;
+    // Área
+    float areaA01, areaA02, areaA03, areaA04;
+    // Número de Pontos Turísticos
+    int numPTA01, numPTA02, numPTA03, numPTA04; 
+     
     return 0;
 }
